0c5bc72e26120a03cb9a61404aff726a1228ecb6
3DJ <71472458+ThreeDeeJay@users.noreply.github.com>
2025-12-27 01:24:01 -0500

    Update build.cmd

--- build.cmd
+++ build.cmd
@@ -273,8 +273,11 @@ REM git -C mesa-%MESA_VERSION% checkout 629a0a4dccd588feb37aa445087386e07aba3f93
 REM 2025-11-07:
 REM git -C mesa-%MESA_VERSION% checkout d3d820d6ef24100515b342041dfa35b14de9d545
 
-REM 2025-08-01:
-git -C mesa-%MESA_VERSION% checkout c64c6a0c31f9cb1339bc700d236932171f7444a3
+REM ~~~~~~~~~~~~~2025-08-01:
+REM ~~~~~~~~~~~~~git -C mesa-%MESA_VERSION% checkout c64c6a0c31f9cb1339bc700d236932171f7444a3
+
+REM 2025-08-06 25.2.5:
+git -C mesa-%MESA_VERSION% checkout 12a4d6858055a536f0efd95d7760c8f496c187bf
 
 REM ----------2025-09-01:
 REM ----------git -C mesa-%MESA_VERSION% checkout 564b6fd7f43c4f1b158cb89251d6f523c1acdeca
